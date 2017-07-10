cced77814cd3f984a516178ba2c08f18e5dc37a4
upforce4 <upforce4@192.168.0.105>
2017-07-10 17:34:03 +0530

    code updated

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 import App from './App';
-import Gallary from './Gallery.js';
+import Gallery from './Gallery.js';
 import Blog from './Blog.js';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -15,7 +15,7 @@ const AppRoute = () => (
                     <ul className="nav navbar-nav">
                         <li><Link to="/minimap-react/">Home</Link></li>
                         <li><Link to="/minimap-react/blog">Blog</Link></li>
-                        <li><Link to="/minimap-react/gallary">Gallary</Link></li>
+                        <li><Link to="/minimap-react/gallery">Gallery</Link></li>
                     </ul>
                 </div>
             </div>
@@ -23,7 +23,7 @@ const AppRoute = () => (
         <div className="container">
           <Route exact path="/minimap-react/" component={App}/>
           <Route path="/minimap-react/blog" component={Blog}/>
-          <Route path="/minimap-react/gallary" component={Gallary}/>
+          <Route path="/minimap-react/gallery" component={Gallery}/>
         </div>
     </div>
   </Router>
